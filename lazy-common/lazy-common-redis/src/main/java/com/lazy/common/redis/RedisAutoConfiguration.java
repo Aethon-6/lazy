8d705cc4a92399d10e61c74ee23958cde4e9980b
ywx <yangwx@dream-it.cn>
2024-10-14 18:43:50 +0800

    redis、redisson

---
+++ lazy-common/lazy-common-redis/src/main/java/com/lazy/common/redis/RedisAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.lazy.common.redis;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : yangwx
+ * @description :
+ * @createDate : 2024/10/14
+ */
+@Configuration
+public class RedisAutoConfiguration {
+}
