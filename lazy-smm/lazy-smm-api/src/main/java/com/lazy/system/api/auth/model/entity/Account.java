c1eb32e0dd06f2bd7068ef35e6623a2fa7ec862a
Aethon-6 <yang.wx.soft@outlook.com>
2024-10-15 23:21:20 +0800

    mybatis plus insert、update fill

--- lazy-smm/lazy-smm-api/src/main/java/com/lazy/system/api/auth/model/entity/Account.java
+++ lazy-smm/lazy-smm-api/src/main/java/com/lazy/system/api/auth/model/entity/Account.java
@@ -1,12 +1,10 @@
 package com.lazy.system.api.auth.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -52,25 +50,25 @@ public class Account implements Serializable {
     /**
      * 创建时间
      */
-    @TableField("create_time")
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
     /**
      * 创建人
      */
-    @TableField("create_by")
+    @TableField(value = "create_by", fill = FieldFill.INSERT)
     private Long createBy;
 
     /**
      * 修改时间
      */
-    @TableField("update_time")
+    @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
     /**
      * 修改人
      */
-    @TableField("update_by")
+    @TableField(value = "update_by", fill = FieldFill.INSERT_UPDATE)
     private Long updateBy;
 
     /**
